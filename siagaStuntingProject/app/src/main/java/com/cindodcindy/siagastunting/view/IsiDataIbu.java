01759e01b4accdb72fbb746fce2ddf5b4575c716
CindoddCindy <cindodcindy@gmail.com>
2021-07-11 15:49:07 +0700

    tambah data asset

---
+++ siagaStuntingProject/app/src/main/java/com/cindodcindy/siagastunting/view/IsiDataIbu.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.siagastunting.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.siagastunting.R;
+
+public class IsiDataIbu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isi_data_ibu);
+    }
+}
---
+++ siagaStuntingProject/app/src/main/java/com/cindodcindy/siagastunting/view/PilihMenu.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.siagastunting.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.siagastunting.R;
+
+public class PilihMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_menu);
+    }
+}
